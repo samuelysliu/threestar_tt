0b6b0cd68a0002a44cf125f8648c5e970c77e645
samuelbarry <samuel870822@gmail.com>
2022-06-30 10:51:35 +0800

    add icon and app name

--- templates/src/components/header.js
+++ templates/src/components/header.js
@@ -45,7 +45,7 @@ function Header({ sendUserInfo }) {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="">Three star TT</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
