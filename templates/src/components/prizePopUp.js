b05d42a825502337150b1f0ab53f2400ee3365ed
sasakevin <sasakevin7@outlook.com>
2022-08-22 15:49:07 +0800

    temp 0822 03:49

--- templates/src/components/prizePopUp.js
+++ templates/src/components/prizePopUp.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Row, Col } from 'react-bootstrap';
 import PrizePopUpIcon from '../images/prizePopUpIcon.png'
 import '../styles/prizePopUp.css'
+import threeStarTokenIcon2 from '../images/3StarToken2.png'
+import TTTokenIcon2 from '../images/TTToken2.png'
 
 function PrizePopUp() {
     const [show, setShow] = useState(true);
@@ -13,24 +15,31 @@ function PrizePopUp() {
         <>
             <Modal show={show} onHide={handleClose} centered>
                 <div className='modalStyle'>
-                <img src={PrizePopUpIcon} alt="prizePopUp" width="80px" className='popUpIcon'></img>
-                <div className='modalHeader'>
-                    <font style={{fontSize: "12px"}}>DAILY BONUS</font>
-                    <br></br>
-                    <font style={{fontSize: "20px"}}>DOUBLE REWARD</font>
+                    <img src={PrizePopUpIcon} alt="prizePopUp" width="80px" className='popUpIcon'></img>
+                    <div className='modalHeader'>
+                        <font style={{ fontSize: "12px" }}>DAILY BONUS</font>
+                        <br></br>
+                        <font style={{ fontSize: "20px" }}>DOUBLE REWARD</font>
+                    </div>
+
+                    <div className='modalContent'>
+                        <Row>
+                            <Col><font>Claim once a day.<br></br>Make you win DOUBLE reward.</font></Col>
+                        </Row>
+                        <Row>
+                            <Col xs={5} style={{textAlign:"right"}}><img src={TTTokenIcon2}></img></Col>
+                            <Col xs={1}><div style={{ borderLeft: "1px solid #0CB1FD", height: "100%" }}></div></Col>
+                            <Col xs={5} style={{textAlign:"left"}}><img src={threeStarTokenIcon2}></img></Col>
+                        </Row>
+                    </div>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <button onClick={handleClose}>
+                            Close
+                        </button>
+                    </Modal.Footer>
                 </div>
-                
-                <div className='modalContent'>
-                    <font>Claim once a day.<br></br>Make you win DOUBLE reward.</font>
-                </div>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <button onClick={handleClose}>
-                        Close
-                    </button>
-                </Modal.Footer>
-                </div>
-                
+
             </Modal>
         </>
     );
