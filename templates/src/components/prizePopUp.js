ca3cc06b2b4f3a0c6bccf2b950df81486829cfce
sasakevin <sasakevin7@outlook.com>
2022-08-22 15:59:56 +0800

    temp 0822 03:59

--- templates/src/components/prizePopUp.js
+++ templates/src/components/prizePopUp.js
@@ -26,10 +26,15 @@ function PrizePopUp() {
                         <Row>
                             <Col><font>Claim once a day.<br></br>Make you win DOUBLE reward.</font></Col>
                         </Row>
-                        <Row>
+                        <Row style={{justifyContent:"center"}}>
                             <Col xs={5} style={{textAlign:"right"}}><img src={TTTokenIcon2}></img></Col>
                             <Col xs={1}><div style={{ borderLeft: "1px solid #0CB1FD", height: "100%" }}></div></Col>
                             <Col xs={5} style={{textAlign:"left"}}><img src={threeStarTokenIcon2}></img></Col>
+                        </Row>
+                        <Row style={{padding: "9px"}}>
+                            <Col>
+                                <font>Spend your original BET and get <strong>TWICE</strong> as much TT or 3Star.</font>
+                            </Col>
                         </Row>
                     </div>
                     <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
