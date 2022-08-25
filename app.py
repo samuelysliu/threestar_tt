540709edd48e5e4c5d2437be9c302d2df51ccac9
sasakevin <sasakevin7@outlook.com>
2022-08-25 17:09:09 +0800

    v2 daily bonus coupon

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import datetime
 load_dotenv()
 
 app = Flask(__name__, static_folder='templates/build')
-# CORS(app, resources={"/api/.*": {"origins": [os.getenv("REACT_APP_APIPATH")]}})
-# CORS(app, resources={"/bsc/.*": {"origins": [os.getenv("REACT_APP_APIPATH")]}})
+CORS(app, resources={"/api/.*": {"origins": [os.getenv("REACT_APP_APIPATH")]}})
+CORS(app, resources={"/bsc/.*": {"origins": [os.getenv("REACT_APP_APIPATH")]}})
 CORS(app, resources={"/master/.*": {"origins": ["192.168.100.10"]}})
-CORS(app)
+# CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['PROPAGATE_EXCEPTIONS'] = True
