dd0dd3dbc69826aabe1edb00efceeacc1ef52d1b
sasakevin <sasakevin7@outlook.com>
2022-08-27 09:30:37 +0800

    open scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,9 @@ from control.threeStar import threeStar
 threeStar_tt = threeStar("thunderCore")
 threeStar_bsc = threeStar("bsc")
 
-#threeStar_tt.saveLastRound()
+threeStar_tt.saveLastRound()
 threeStar_tt.setReward()
-#threeStar_bsc.saveLastRound()
-#threeStar_bsc.setReward()
+threeStar_bsc.saveLastRound()
+threeStar_bsc.setReward()
 
 # threeStar.test()
